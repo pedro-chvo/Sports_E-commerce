e7ebc013225709f765c9fc74eb662e1562e7899c
pedro-chvo <pedrort872@gmail.com>
2026-04-26 19:08:28 -0600

    feat: servir paginas estaticas con express

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const productoRoutes = require('./src/routes/producto.routes');
@@ -8,13 +9,12 @@ const authRoutes = require('./src/routes/user.routes');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+const frontendPath = path.resolve(__dirname, '../frontend');
+console.log('Sirviendo frontend desde:', frontendPath);
+app.use(express.static(frontendPath));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/productos', productoRoutes);
-
-app.get('/', (req, res) => {
-    res.json({ mensaje: 'API Sports E-commerce funcionando' });
-});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
